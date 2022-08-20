7a1dfed8801a42df0155659cd4e324a3dc6d8454
bloxain <100310884+bloxain@users.noreply.github.com>
2022-08-20 09:51:17 -0700

    main script

---
+++ Extra/Videos/alt control.lua
@@ -0,0 +1,9 @@
+--[[
+UPDATED LOADSTRINGS
+USE OLD LINKS IF THESE DONT WORK
+--]]
+
+--For Bots(put in autoexec)
+loadstring(game:HttpGet("https://github.com/bloxain/Roblox/raw/main/Extra/Videos/bot.lua"))()
+--for controler(exec like normal)
+loadstring(game:HttpGet("https://github.com/bloxain/Roblox/raw/main/Extra/Videos/control.lua"))()
