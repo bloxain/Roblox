5d2f54812addbb2e68813ea85e1aa5daaa02782f
bloxain <100310884+bloxain@users.noreply.github.com>
2023-03-30 19:00:52 -0700

    new game

---
+++ Games/B/Boxing_League.lua
@@ -0,0 +1,9 @@
+repeat task.wait() until game:IsLoaded()
+
+setclipboard("https://discord.gg/PPFYacGb2b")
+print("Join our server! https://discord.gg/PPFYacGb2b", game.PlaceId)
+if game.PlaceId == 3738115442 then
+    loadstring(game:HttpGet("https://github.com/CPU-Accelerator/Boxing-League/raw/main/Boxing-Classes/ClassC.lua", true))()
+elseif game.PlaceId == 4574912733 then
+    loadstring(game:HttpGet("https://github.com/CPU-Accelerator/Boxing-League/raw/main/Boxing-Classes/ClassB.lua", true))()
+end
