35d151a44bff2ac4c0687e1e056f03e95bfe85f5
CPU-Accelerator <99703953+CPU-Accelerator@users.noreply.github.com>
2023-04-08 15:20:51 -0800

    Update ClassLoader.lua

--- Games/B/ClassLoader.lua
+++ Games/B/ClassLoader.lua
@@ -2,7 +2,7 @@ repeat task.wait() until game:IsLoaded()
 print("Join our server! https://discord.gg/PPFYacGb2b")
 
 if game.PlaceId == 3738115442 then
-	loadstring(game:HttpGet(("https://raw.githubusercontent.com/CPU-Accelerator/Boxing-League/main/Boxing-Classes/ClassC.lua"), true))()
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/bloxain/Roblox/main/Games/B/Boxing%20League/ClassC.lua"), true))()
 elseif game.PlaceId == 4574912733 then
-	loadstring(game:HttpGet(("https://raw.githubusercontent.com/CPU-Accelerator/Boxing-League/main/Boxing-Classes/ClassB.lua"), true))()
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/bloxain/Roblox/main/Games/B/Boxing%20League/ClassB.lua"), true))()
 end
