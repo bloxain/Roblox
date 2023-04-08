0e78d9e5f24105e5616455ab50d813936196e293
bloxain <100310884+bloxain@users.noreply.github.com>
2023-04-08 16:41:54 -0700

    Update ClassLoader.lua

--- Games/B/ClassLoader.lua
+++ Games/B/ClassLoader.lua
@@ -2,7 +2,7 @@ repeat task.wait() until game:IsLoaded()
 print("Join our server! https://discord.gg/PPFYacGb2b")
 
 if game.PlaceId == 3738115442 then
-	loadstring(game:HttpGet(("https://raw.githubusercontent.com/bloxain/Roblox/main/Games/B/Boxing%20League/ClassC.lua"), true))()
+	loadstring(game:HttpGet(("https://github.com/bloxain/Roblox/raw/main/Games/B/ClassB.lua"), true))()
 elseif game.PlaceId == 4574912733 then
 	loadstring(game:HttpGet(("https://raw.githubusercontent.com/bloxain/Roblox/main/Games/B/Boxing%20League/ClassB.lua"), true))()
 end
