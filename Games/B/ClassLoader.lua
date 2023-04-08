6147110a235c1d1f8ea266d78098264ebd8165b7
bloxain <100310884+bloxain@users.noreply.github.com>
2023-04-08 16:44:58 -0700

    Update ClassLoader.lua

--- Games/B/ClassLoader.lua
+++ Games/B/ClassLoader.lua
@@ -2,7 +2,7 @@ repeat task.wait() until game:IsLoaded()
 print("Join our server! https://discord.gg/PPFYacGb2b")
 
 if game.PlaceId == 3738115442 then
-	loadstring(game:HttpGet(("https://github.com/bloxain/Roblox/raw/main/Games/B/ClassB.lua"), true))()
+	loadstring(game:HttpGet("https://github.com/bloxain/Roblox/raw/main/Games/B/ClassB.lua"), true))()
 elseif game.PlaceId == 4574912733 then
-	loadstring(game:HttpGet(("https://github.com/bloxain/Roblox/raw/main/Games/B/ClassC.lua"), true))()
+	loadstring(game:HttpGet("https://github.com/bloxain/Roblox/raw/main/Games/B/ClassC.lua"), true))()
 end
