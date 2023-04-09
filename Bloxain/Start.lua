26abf171cd2de365e0e450c518b482c4f5eebb98
bloxain <100310884+bloxain@users.noreply.github.com>
2023-04-08 21:17:49 -0700

    Update Start.lua

--- Bloxain/Start.lua
+++ Bloxain/Start.lua
@@ -1,6 +1,6 @@
-_G.BloxainSettings = {Path = 'Bloxain', Games = 'Bloxain/Games', Modules = 'Bloxain/Modules', WebAddress = 'https://github.com/bloxain/Roblox/raw/Beta/Bloxain/'}
+local MainDir = 'Bloxain'
+_G.BloxainSettings = {Path = MainDir, Games = MainDir..'/Games', Modules = MainDir..'/Modules', WebAddress = 'https://github.com/bloxain/Roblox/raw/Beta/Bloxain/', OldUpdate = MainDir..'/Update.JSON'}
 local Modules = _G.BloxainSettings.Modules
 local WebAddress = _G.BloxainSettings.WebAddress
-syn.request({Url = WebAddress, Body = 'Started', Method = "POST"})
-loadstring(syn.request({Url = WebAddress..Modules..'/Updater.lua', Method = "GET"}).Body)()
+pcall(function() loadstring(syn.request({Url = WebAddress..Modules..'/Updater.lua', Method = "GET"}).Body)() end)
 loadstring(readfile(Modules..'/Game_Finder.Lua'))()
