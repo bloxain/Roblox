f07b70a50aa5e0d2336e83d123e7e8f07c62eb0e
bloxain <100310884+bloxain@users.noreply.github.com>
2023-04-08 21:09:02 -0700

    Delete Games directory

--- Games/B/ClassLoader.lua
+++
@@ -1,8 +0,0 @@
-repeat task.wait() until game:IsLoaded()
-print("Join our server! https://discord.gg/PPFYacGb2b")
-
-if game.PlaceId == 3738115442 then
-	loadstring(game:HttpGet("https://github.com/bloxain/Roblox/raw/main/Games/B/ClassB.lua"), true))()
-elseif game.PlaceId == 4574912733 then
-	loadstring(game:HttpGet("https://github.com/bloxain/Roblox/raw/main/Games/B/ClassC.lua"), true))()
-end
