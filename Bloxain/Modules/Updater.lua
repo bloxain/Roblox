877cda85532c076bf11783f68f3ca41447f78b65
bloxain <100310884+bloxain@users.noreply.github.com>
2023-04-10 17:26:28 -0700

    Update Updater.lua

--- Bloxain/Modules/Updater.lua
+++ Bloxain/Modules/Updater.lua
@@ -1,5 +1,6 @@
 local Path = _G.BloxainSettings.Path
 local WebAddress = _G.BloxainSettings.WebAddress
+local Modules = _G.BloxainSettings.Modules
 
 local Folders = {
 	'/Games',
@@ -7,10 +8,10 @@ local Folders = {
 }
 
 local Files = {
-	{'/Universal_Gui.lua', 1},
-	{'/Modules/Updater.lua', 1},
-	{'/Modules/UI.Lua', 1},
-	{'/Modules/Game_Finder.Lua', 1}
+	{'Universal_Gui.lua', 1},
+	{'Modules/Updater.lua', 1},
+	{'Modules/UI.Lua', 1},
+	{'Modules/Game_Finder.Lua', 1}
 }
 
 local function Update()
@@ -23,11 +24,11 @@ local function Update()
 	
 	for _, file in next, Files do
 		spawn(function()
-			writefileasync(Path..file[1], syn.request({Url = WebAddress..file[1]..tostring(game.GameId)..'.lua', Method = "GET"}).Body)
+			writefileasync(Path..'/'..file[1], syn.request({Url = WebAddress..file[1], Method = "GET"}).Body)
 		end)
 	end
 	
-	local GameFile = writefileasync(Path..'/Games/'..tostring(game.GameId)..'.lua', syn.request({Url = WebAddress..'Games/'..tostring(game.GameId)..'.lua', Method = "GET"}).Body)
+	local GameFile = writefileasync(Path..'/Games/'..tostring(game.GameId)..'.lua', syn.request({Url = WebAddress..Modules..'Games/'..tostring(game.GameId)..'.lua', Method = "GET"}).Body)
 end
 
 Update()
